36bba1e5fecd23a921c82e6cea1565d5a04ab6a1
AllysonAndreaFierro <42786340+AllysonAndreaFierro@users.noreply.github.com>
2018-10-18 11:34:01 -0400

    built ui sidebar and body

--- app.R
+++ app.R
@@ -13,19 +13,28 @@ library(plotly)
 schools <- rgdal::readOGR("https://opendata.arcgis.com/datasets/70baf6da243e40298ba9246e9a67409b_0.geojson") %>%
   spTransform(CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   # Application title
-  header <- dashboardHeader(title = "Los Angeles County..."),
+# Define UI
+  header <- dashboardHeader(title = "Los Angeles County...")
   
+  sidebar <- dashboardSidebar(
+    sidebarMenu(
+      id = "tabs",
+      menuItem("Map", tabName = "Map", icon = icon("map-marker")),
+      menuItem("Chart", tabName = "Chart", icon = icon("bar-chart")),
+      menuItem("Table", tabName = "Table", icon = icon("table"))
+    )
+  )
+  
+  body<- dashboardBody(
   fluidRow(
     box(
       leafletOutput("schoolmap")
     )
   )
-
 )
-# Define server logic required to draw a histogram
+
+ui <- dashboardPage(header, sidebar, body)
+# Define server logic
 server <- function(input, output) {
   output$schoolmap <- renderLeaflet({
     leaflet(schools) %>%
